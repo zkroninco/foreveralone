67a7d5aad597cb9687f57017e28654fe2917b28b
zkronin <zkronin@example.com>
2025-12-02 14:38:10 -0600

    fix: Correct build configuration for Netlify

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,4 @@
-/// <reference types="vitest/globals" />
-
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
